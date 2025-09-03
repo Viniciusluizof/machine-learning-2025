e8ca73e44f3568c43c305c6516327e66bb833417
Vinicius Farias <viniciusluizof@gmail.com>
2025-09-03 15:34:33 -0300

    feat: regressão

--- regressao.py
+++ regressao.py
@@ -6,7 +6,7 @@ df = pd.read_excel("data/dados_cerveja_nota.xlsx")
 df.head()
 # %%
 from sklearn import linear_model
-
+from sklearn import tree
 X = df[['cerveja']] # Isso é uma matriz (df)
 y = df['nota'] # isso é um vetor (series)
 
@@ -14,14 +14,18 @@ y = df['nota'] # isso é um vetor (series)
 
 reg = linear_model.LinearRegression(fit_intercept=True)
 reg.fit(X,y)
-# %%
-reg.coef_
-# %%
-a,b = reg.intercept_, reg.coef_[0]
 
-print(a,b)
-# %%
-predict = reg.predict(X.drop_duplicates())
+a,b = reg.intercept_, reg.coef_[0]
+predict_reg = reg.predict(X.drop_duplicates())
+
+arvore_full = tree.DecisionTreeRegressor(random_state=42)
+arvore_full.fit(X,y)
+predict_arvore_full = arvore_full.predict(X.drop_duplicates())
+
+arvore_d2 = tree.DecisionTreeRegressor(random_state=42, 
+                                       max_depth=2)
+arvore_d2.fit(X,y)
+predict_arvore_d2 = arvore_d2.predict(X.drop_duplicates())
 
 # %%
 
@@ -33,8 +37,20 @@ plt.title("Relação Cerveja vs Nota")
 plt.xlabel("Cerveja")
 plt.ylabel("Nota")
 plt.plot(X.drop_duplicates()['cerveja'],
-         predict)
-plt.legend(['Observado', f'y = {a:.3f} + {b:.3f}x'])
+         predict_reg)
 
+plt.plot(X.drop_duplicates()['cerveja'],predict_arvore_full)
+plt.plot(X.drop_duplicates()['cerveja'],predict_arvore_d2)
+
+plt.legend(['Observado', 
+            f'y = {a:.3f} + {b:.3f}x',
+            'Arvore Full',
+            'Arvore Depth = 2'])
 plt.show()
+# %%
+
+plt.figure(dpi=400)
+tree.plot_tree(arvore_d2,
+               feature_names=['cerveja'],
+               filled=True)
 # %%
